74eb49ac2d7591e0547d7cec2731e154f5f2f159
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-22 13:09:26 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,10 @@ typedef struct binary_tree_s
 
 } binary_tree_t;
 
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
+
+/* function prototypes for tasks*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
