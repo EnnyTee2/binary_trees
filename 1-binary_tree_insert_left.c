77e2093f15cc0e05f963f911455f514ae37193f8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-22 16:56:39 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	binary_tree_t *temp;
+	binary_tree_t temp;
 
 	if (parent == NULL)
 		return NULL;
