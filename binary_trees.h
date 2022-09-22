df3a73bf8b08c017dc3e049c8a9a9b2d000f3c3e
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-22 12:46:30 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
  * struct binary_tree_s - binary tree definition
@@ -26,3 +25,5 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
