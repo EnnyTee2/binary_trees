a2cdeb8065396a01c1cc2bf497cc7e84beae5127
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-22 12:04:10 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,13 @@
 
+#include "binary_trees.h"
+
+/**
+ * struct binary_tree_s - binary tree definition
+ * @n: integer
+ * @root: points to the root node
+ * @left: points to the left child
+ * @right: points to the right child
+ *
+ * Description: binary tree node structure
+ *
+ */
