fe4ca221d8566a1adf0abee334481df44e0db011
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-22 12:29:18 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,3 +11,17 @@
  * Description: binary tree node structure
  *
  */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+	
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		/* if malloc is not successful*/
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+}
